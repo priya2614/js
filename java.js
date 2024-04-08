56d69b8d4cc0139e0a1db123c2d0274fcbdfdf95
priya2614 <165402467+priya2614@users.noreply.github.com>
2024-04-08 06:36:06 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,147 @@
+
+
+var myInfo = {
+    firstName:'Priya',
+    lastName:'Dema',
+    location:'Andhra Pradesh',
+    fatherName:'Dema Sayanna',
+    motherName:'Dema Rajeswari',
+
+}
+console.log(myInfo);
+
+ var study = {
+    schoolName:'Surya VidyaNikethan',
+    marks:'9.3',
+    collegeName:'Vijaya Co-op Juniour College',
+    percentage:'80%',
+    underGraduation:'Sri Sai chaithaya degree college',
+    percent:'85%',
+ }
+ console.log(study);
+
+ var favPlaces = {
+    placeGoa:'Goa',
+    placeKerala:'kerala',
+    placeOoty:'Ooty',
+    placeGokarna:'',
+    placeAgra:'Taj Mahal',
+    
+ }
+ console.log(favPlaces);
+  
+ var information = ['priya', 21,'Giddalur', 523357, 'kanna']; //array
+
+ console.log(information);
+
+ var mass = [ 230, 'rajesh', false,];
+console.log(mass);
+ 
+
+ var num1 = 250;
+ var num2 = 300;
+ var add = num1+num2; //addition
+console.log(add);
+
+let num3 = 1000;
+let num4 = 2.7;
+let sum = num3+num4; //sum
+console.log(sum);
+
+let num5 = 590;
+let num6 = 290;
+let minus = num5-num6;  //minus
+console.log(minus);
+
+let num7 = 60;
+let num8 = 250;
+let multiply = num7*num8;  //multiplication
+console.log(multiply);
+ 
+let num9 = 87;
+let num10 = 5;
+let divide = num9/num10;  //divison
+console.log(divide);
+
+let num11 = 290;
+let num12 = 89;
+let per = num11%num12;  //remainder
+console.log(per);
+
+let exnum1 = 6;
+let exnum2 = 5;
+let totalex = exnum1**exnum2;  //exponential
+console.log(totalex);
+
+let rollnum = 26 ;
+let name =   '  priya Dema ' ;
+let frnd = '  divya';
+let myDetails = rollnum + name + frnd;  //  conversion
+console.log(myDetails);
+console.log(typeof(myDetails));
+
+// letn3  = 20;
+// letn4 = 'priya';
+// numn4 = Number(n4);
+// console.log(numn4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
